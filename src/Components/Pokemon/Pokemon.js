6f502674a720042cbc69237b751ea65b350540ab
cryptospider937 <mihir.mahakalkar@gmail.com>
2024-06-14 22:48:50 +0530

    feat: added loader

--- src/Components/Pokemon/Pokemon.js
+++ src/Components/Pokemon/Pokemon.js
@@ -1,17 +1,21 @@
 import { useState, useEffect } from "react";
 import { getDetails } from "../../services/utils";
 import { typeColor } from "../../constants";
+import Loader from "../Loader/Loader";
 import "./Pokemon.css";
 
 const Pokemon = ({ name, url }) => {
   const [details, setDetails] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
   const pokeName = name[0].toUpperCase() + name.slice(1);
 
   useEffect(() => {
     const fetchDataFromAPI = async (url) => {
       try {
+        setIsLoading(true);
         const result = await getDetails(url);
         setDetails(result);
+        setIsLoading(false);
       } catch (error) {
         console.error("Error fetching data:", error);
       }
@@ -22,7 +26,7 @@ const Pokemon = ({ name, url }) => {
 
   return details ? (
     <div
-      className="card"
+      className={`card ${ isLoading ? "loading" : ""}`}
       style={{
         background: `radial-gradient(
       circle at 50% 0%, ${
@@ -30,45 +34,53 @@ const Pokemon = ({ name, url }) => {
       } 36%, #efffff 36%)`,
       }}
     >
-      <div className="row">
-        <p className="rank">
-          <span>Rank</span> {details.order}
-        </p>
-        <p className="hp">
-          <span>HP</span> {details.stats[0].base_stat}
-        </p>
-      </div>
-      <img
-        src={details.sprites.other.dream_world.front_default}
-        alt={pokeName}
-      />
-      <h2 className="poke-name">{pokeName}</h2>
-      <div className="types">
-        {details.types.map((item, index) => (
-          <span
-            key={index}
-            style={{ background: `${typeColor[details.types[0].type.name]}` }}
-          >
-            {item.type.name}
-          </span>
-        ))}
-      </div>
-      <div className="stats">
-        <div>
-          <h3>{details.stats[1].base_stat}</h3>
-          <p>Attack</p>
-        </div>
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <>
+          <div className="row">
+            <p className="rank">
+              <span>Rank</span> {details.order}
+            </p>
+            <p className="hp">
+              <span>HP</span> {details.stats[0].base_stat}
+            </p>
+          </div>
+          <img
+            src={details.sprites.other.dream_world.front_default}
+            alt={pokeName}
+          />
+          <h2 className="poke-name">{pokeName}</h2>
+          <div className="types">
+            {details.types.map((item, index) => (
+              <span
+                key={index}
+                style={{
+                  background: `${typeColor[details.types[0].type.name]}`,
+                }}
+              >
+                {item.type.name}
+              </span>
+            ))}
+          </div>
+          <div className="stats">
+            <div>
+              <h3>{details.stats[1].base_stat}</h3>
+              <p>Attack</p>
+            </div>
 
-        <div>
-          <h3>{details.stats[2].base_stat}</h3>
-          <p>Defense</p>
-        </div>
+            <div>
+              <h3>{details.stats[2].base_stat}</h3>
+              <p>Defense</p>
+            </div>
 
-        <div>
-          <h3>{details.stats[5].base_stat}</h3>
-          <p>Speed</p>
-        </div>
-      </div>
+            <div>
+              <h3>{details.stats[5].base_stat}</h3>
+              <p>Speed</p>
+            </div>
+          </div>
+        </>
+      )}
     </div>
   ) : null;
 };
